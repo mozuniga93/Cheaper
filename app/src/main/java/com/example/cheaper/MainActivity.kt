da8b932313d19e6c4fcbdfac777175169959ce3a
mozuniga93 <mzunigaa@ucenfotec.ac.cr>
2021-10-25 21:14:11 -0600

    Cheaper app creation

---
+++ app/src/main/java/com/example/cheaper/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.cheaper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.cheaper.fragments.BuscarFragment
+import com.example.cheaper.fragments.FavoritosFragment
+import com.example.cheaper.fragments.InicioFragment
+import com.example.cheaper.fragments.PerfilFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val inicioFragment = InicioFragment()
+        val buscarFragment = BuscarFragment()
+        val favoritosFragment = FavoritosFragment()
+        val perfilFragment = PerfilFragment()
+
+        makeCurrentFragment(inicioFragment)
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.ic_inicio -> makeCurrentFragment(inicioFragment)
+                R.id.ic_buscar -> makeCurrentFragment(buscarFragment)
+                R.id.ic_favoritos -> makeCurrentFragment(favoritosFragment)
+                R.id.ic_perfil -> makeCurrentFragment(perfilFragment)
+            }
+            true
+        }
+    }
+
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
+}
