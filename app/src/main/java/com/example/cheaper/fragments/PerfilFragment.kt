f45e680356dfbb570ebcd59e3fec1435dacf4fa7
erocham <erocham@ucenfotec.ac.cr>
2021-10-28 21:08:46 -0600

    SOmething in profile

--- app/src/main/java/com/example/cheaper/fragments/PerfilFragment.kt
+++ app/src/main/java/com/example/cheaper/fragments/PerfilFragment.kt
@@ -1,11 +1,13 @@
 package com.example.cheaper.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.cheaper.R
+import kotlinx.android.synthetic.main.fragment_perfil.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
